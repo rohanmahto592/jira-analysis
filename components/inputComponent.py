20b3bcf5ccf173d1d6069b5bb270c7905b07d13b
rohan mahto <rohanmahto592@gmail.com>
2024-04-18 09:13:39 +0530

    moved helper functions to util

--- components/inputComponent.py
+++ components/inputComponent.py
@@ -1,24 +1,7 @@
 import streamlit as st
-import time
-from io import BytesIO
-from docx import Document
-def process_prd(upload_file):
-
-    if upload_file is None:
-            st.error("Please Select Doc File")
-    elif upload_file is not None:
-        file_bytes=upload_file.getvalue()
-        try:
-            if(file_bytes is not None):
-                docx =Document(BytesIO(file_bytes))
-                return docx
-                
-        except Exception as e:
-            st.error(f"Error while loading a file \n {e}")
-
-def process_tickets(file_list):
-    st.write("Processing ticket files...")
-
+import sys
+sys.path.append('../')
+from utils.processFile import process_prd,process_tickets
 class InputComponent:
 
     def __init__(self) -> None:
@@ -40,7 +23,7 @@ class InputComponent:
                 ticket_files = st.file_uploader(label="Upload doc files", type=['docx'], accept_multiple_files=True, key="tickets")
                 submit_tickets = st.form_submit_button("Submit Ticket Files")
 
-                if submit_tickets and ticket_files:
+                if submit_tickets and ticket_files is not None:
                     process_tickets(ticket_files)  
 
 ic = InputComponent()
